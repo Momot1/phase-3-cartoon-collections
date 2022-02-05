37a9c998729a5c5f290f8c82fa632423b5d05403
Mark Momot <markmomot03@gmail.com>
2022-02-04 20:51:02 -0500

    commit tree 6fcbc1f546badf5a61616dc86643b30c65dd2b44

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(ary)
+  ary.each_with_index {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(ary)
+  ary.map{|str| str.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(ary)
+  ary.each {|item|
+    if item.length > 4
+      return true
+    end
+}
+  false
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ary)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if ary.include?("cheddar")
+    "cheddar"
+  elsif ary.include?("gouda")
+    "gouda"
+  elsif ary.include?("camembert")
+    "camembert"
+  end
 end
